ccf4fb2fa4d0bd937778fb3732fc1d19ef236940
Chelsea Troy <chelsea.dommert@gmail.com>
2019-04-14 13:58:52 -0400

    Add begin and rollback commands

--- database.rb
+++ database.rb
@@ -3,35 +3,48 @@ require 'singleton'
 class Database
   include Singleton
 
-  attr_accessor :names, :counts
+  attr_accessor :names, :count_versions
 
   def initialize
-    @names = Hash.new()
-    @counts = Hash.new(0)
+    @count_versions = [Hash.new(0)]
+    @db_versions = [Hash.new()]
   end
 
+  # CRUD COMMANDS
   def set(key, value)
-    @names[key] = value
-    @counts[value] += 1
+    @db_versions[-1][key] = value
+    @count_versions[-1][value] += 1
   end
 
   def get(key)
-    value = @names.fetch(key, "NULL")
+    value = @db_versions[-1].fetch(key, "NULL")
     return value
   end
 
   def count(value)
-    count = @counts.fetch(value, 0)
+    count = @count_versions[-1].fetch(value, 0)
     return count
   end
 
   def delete(key)
-    value = @names[key]
+    value = @db_versions[-1][key]
     if value
-      @names.delete(key)
-      @counts[value] -= 1
+      @db_versions[-1].delete(key)
+      @count_versions[-1][value] -= 1
     else
       print("That key isn't in the database!")
     end
   end
+
+  #TRANSACTION MANAGEMENT
+  def begin()
+    @db_versions.push(Hash.new(0))
+    @count_versions.push(Hash.new(0))
+  end
+
+  def rollback()
+    @db_versions.pop
+    @count_versions.pop
+  end
+  
 end
